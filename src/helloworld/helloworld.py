87010c108d0402021fe6559ada365a0bc77f3d8d
unknown <gayatri.chabra@gmail.com>
2024-01-19 16:38:00 -0500

    added my name

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -4,7 +4,10 @@ def main():
     print("""
     Hello World! from:
     - Joel Welling
-    - Gayatri Chabra      
+    - Gayatri Chabra
+    - Rohit Varanasy
+    - Richard Wang
+    - Joon Jung                        
     """)
 
 if __name__ == "__main__":
