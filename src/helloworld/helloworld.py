05f48eae008bf98fcdaa5b909615d1ab43494927
JoelBeltranG <155424057+JoelBeltranG@users.noreply.github.com>
2024-01-23 11:40:35 -0500

    Update helloworld.py with 3 membes
    Joel Ruby and Xinran

--- src/helloworld/helloworld.py
+++ src/helloworld/helloworld.py
@@ -5,11 +5,8 @@ def main():
     Hello World! from group 5:
     - Joel Welling
     - Ruby Shen
-<<<<<<< HEAD
     - Xinran Chen
-=======
     - Joel Beltran
->>>>>>> 572185938929eca2927499db932c2a31cb23e32e
     """)
 
 if __name__ == "__main__":
